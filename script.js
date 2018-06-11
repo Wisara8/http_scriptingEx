9102d8ec2574abd02fa01987a78ff6305acf6890
Wisara8 <srandall845@gmail.com>
2018-06-11 18:56:07 +0000

    Step 1

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-test
+var https = require('https');
+
+
+function getAndPrintHTMLChunks() {
+  var string = "";
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+
+      // string += data.toString();
+      // string += '\n';
+      string += data + '\n';
+    });
+
+    // response.on('end', function() {
+    //   console.log('Response stream complete.');
+    //   return string;
+    // });
+
+  });
+}
+
+getAndPrintHTMLChunks();
